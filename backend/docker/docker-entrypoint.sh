79d3c51a79557abcfaee616f904a67193eccc632
JavierIbarra <jaibarra1@miuandes.cl>
2022-09-26 16:17:00 -0300

    start project

---
+++ backend/docker/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Collect static files
+#echo "Collect static files"
+#python manage.py collectstatic --noinput
+
+echo "Flusing django manage command"
+python manage.py flush --no-input
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver $WEB_PORT
