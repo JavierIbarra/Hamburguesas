b506f31e895116677e827864a28056e57e5a844f
JavierIbarra <jaibarra1@miuandes.cl>
2022-10-02 15:08:33 -0300

    docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ, os
+import environ
 
 env = environ.Env(
     DEBUG=(bool, False)
 )
 
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
