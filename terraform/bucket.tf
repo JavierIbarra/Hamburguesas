c6e3c6d1875fcd3b6b7b9c65a995b0d51a342631
Javier Ibarra <javie@javier-dell.localdomain>
2022-11-12 22:20:59 -0300

    ami-vpc-subnet

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,28 +1,57 @@
-
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
- resource "aws_instance" "ec2" {
-    ami = "ami-08c40ec9ead489470"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-025e03e4152467c2d"
+resource "aws_ami" "ubuntu"{
+  most_recent = true
+
+  filter{
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = []
+}
+
+/* VPC ----------------------------------------------------------------- */
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+/* Subnet ----------------------------------------------------------------- */
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[0].cidr_block
+  availability_zone = "us-west-2"
+
+  tags = {
+    Name = var.subnet_prefix[0].name
+  }
 }
 
 /* Instance ----------------------------------------------------------------- */
 
-resource "aws_instance" "app" {
-  ami             = "ami-0560993025898e8e8" # Amazon Linux 2
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.app.name]
+resource "aws_instance" "ec2" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+    subnet_id = "subnet-025e03e4152467c2d"
+    security_groups = [aws_security_group.app.name]
 
-  tags = {
-    Name = "app"
-  }
-
-  user_data = module.container-server.cloud_config
+    tags = {
+      Name = "app"
+    }
 }
 
 /* DNS ---------------------------------------------------------------------- */
